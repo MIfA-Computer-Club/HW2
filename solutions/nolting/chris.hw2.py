cb65ee79343a2595fd231becc8e0ffc285314ef0
Crazenhaif <nolting@astro.umn.edu>
2014-07-23 22:35:24 -0500

    finished radial profile and half-light radius

--- solutions/nolting/chris.hw2.py
+++ solutions/nolting/chris.hw2.py
@@ -6,9 +6,9 @@ import pyregion
 import matplotlib.cm as cm
 import math
 
-info = 0
+info = 1
 
-def centroid(center_xy, radius,dat):
+def centroid_bright(center_xy, radius,dat):
 	center_x, center_y = center_xy
 	tempMax = 0
 	tempMax2 = 0
@@ -32,53 +32,147 @@ def centroid(center_xy, radius,dat):
 	maximum = maxArrayS[0]
 	newCenter = maxCoordsS[0]
 	
-	corners = [dat[center_y-radius,center_x-radius],dat[center_y-radius,center_x+radius],dat[center_y+radius, center_x-radius],dat[center_y+radius, center_x+radius]]
-	minBack = min(corners)
-	tempback = 0
-	backcounter = 0
-	for corner in corners:
-		if corner < 1.5*sum(corners)/4.0:
-			tempback += corner
-			backcounter +=1
-	background = tempback/backcounter
+	return newCenter
+
+def getBackground(center_xy, radius, dat):
+	center_x, center_y = center_xy
+	grid = dat[center_y-radius:center_y+radius,center_x-radius:center_x+radius]
+	xCoord, yCoord = numpy.mgrid[center_x - radius:center_x + radius, center_y - radius: center_y+radius]
+	radArray = ((xCoord-center_x)**2+(yCoord - center_y)**2)**(0.5)
+	bgGrid = dat[center_y-1.3*radius:center_y+1.3*radius,center_x-1.3*radius:center_x+1.3*radius]
+	backgroundAnnulusMask = numpy.logical_and(radArray>radius*1.1, radArray<radius*1.2)
+	backgroundX, backgroundY = backgroundAnnulusMask.nonzero()
+	background = numpy.median(bgGrid[backgroundY,backgroundX])
+	
+	return background
+
+def getRadius(center_xy, radius,dat):
+	center_x, center_y = center_xy
+	centerVal = dat[center_y,center_x]
+		
+	grid = dat[center_y-radius:center_y+radius,center_x-radius:center_x+radius]
+	xCoord, yCoord = numpy.mgrid[center_x - radius:center_x + radius, center_y - radius: center_y+radius]
+	radArray = ((xCoord-center_x)**2+(yCoord - center_y)**2)**(0.5)
+	
+	background = getBackground(center_xy, radius, dat)
+
+	for x in range(int(round(radius))):
+		mask = numpy.logical_and(radArray<=x, radArray>x-1) ## find all within this annulus
+		maskX, maskY = mask.nonzero()
+		annulusAverage = 0
+		if(len(grid[maskY,maskX])!=0):
+			annulusAverage = sum(grid[maskY,maskX])/len(grid[maskY,maskX])
+		if((annulusAverage - background) > (centerVal - background)/math.e**3):
+			newRadius = x+1
+
+	return newRadius
+
+def makeAp(center_xy, radius, dat):
+	center_x, center_y = center_xy
+	grid = dat[center_y-radius:center_y+radius,center_x-radius:center_x+radius]
+	xCoord, yCoord = numpy.mgrid[center_x - radius:center_x + radius, center_y - radius: center_y+radius]
+	radArray = ((xCoord-center_x)**2+(yCoord - center_y)**2)**(0.5)
+	mask = radArray <= radius
+	maskX, maskY = mask.nonzero()
+	aperature = grid[maskY, maskX]
+	return aperature
+
+def centroid_weighted(center_xy, radius,dat):
+	center_x, center_y = center_xy
+	sumWeightedX = 0
+	sumWeightedY = 0
+	sumWeights = 0
+	
+	background = getBackground(center_xy, radius, dat)
+	
+	for x in range(int(round(center_x - radius)), int(round(center_x + radius))):
+		for y in range(int(round(center_y - radius)), int(round(center_y + radius))):
+			weight = dat[y,x] - background
+			sumWeightedX += weight * x
+			sumWeightedY += weight * y
+			sumWeights += weight
+	newCenter = sumWeightedX/sumWeights, sumWeightedY/sumWeights
+	return newCenter
+	
+## calculate the flux per pixel
+def getFlux(center, radius, dat):
+	center_x, center_y = center
+	background = getBackground(center,radius,dat)
+	flux = 0
+	fluxValues = makeAp(center, radius, dat) - background
+	flux = sum(fluxValues)/len(fluxValues)
+	return flux
+	
+def makeProfile(center_xy, radius, dat):
+	center_x, center_y = center_xy
+	centerVal = dat[center_y,center_x]
+	
+	grid = dat[center_y-radius:center_y+radius,center_x-radius:center_x+radius]
+	xCoord, yCoord = numpy.mgrid[center_x - radius:center_x + radius, center_y - radius: center_y+radius]
+	radArray = ((xCoord-center_x)**2+(yCoord - center_y)**2)**(0.5)
+	background = getBackground(center_xy, radius, dat)
+	
+	annulusAverage = numpy.zeros(int(round(radius)))
+	error = numpy.zeros(int(round(radius)))
+	radiusArray = numpy.zeros(int(round(radius)))
 	
 	for x in range(int(round(radius))):
-		if (dat[newCenter[1],newCenter[0]+x] - background) > (maximum-background)/math.e**3:
-			newRadius1 = x+1
-		
-		if (dat[newCenter[1],newCenter[0]-x] - background) > (maximum-background)/math.e**3:
-			newRadius2 = x+1
-		
-		if (dat[newCenter[1]+x,newCenter[0]] - background) > (maximum-background)/math.e**3:
-			newRadius3 = x+1
-		
-		if (dat[newCenter[1]-x,newCenter[0]] - background) > (maximum-background)/math.e**3:
-			newRadius4 = x+1
-		
-	newRadius = (newRadius1 + newRadius2 + newRadius3 + newRadius4)/4.0
-	print 'newRadius is ', newRadius
-	return newCenter, newRadius
-
+		mask = numpy.logical_and(radArray<=x, radArray>x-1) ## find all within this annulus
+		maskX, maskY = mask.nonzero()
+		if(len(grid[maskY,maskX])!=0):
+			annulusAverage[x] = sum(grid[maskY,maskX])/len(grid[maskY,maskX])
+		error[x] = numpy.std(grid[maskY,maskX])
+		radiusArray[x] = x
+	return annulusAverage,error, radiusArray
+	
 def main():
 
 	coma = pyfits.open('POSIIF_Coma.fits')
 	head = coma[0].header
 	dat = coma[0].data
-
+	
+	scale = head['PLTSCALE'] ##arcsec per mm
+	sizeX = head['PLTSIZEX'] ##mm in x
+	pixX = head['NAXIS1'] ##number of pixels in x
+	sizeY = head['PLTSIZEY'] ##mm in y
+	pixY = head['NAXIS2'] ##number of pixels in y
+	pix2Permm2 = pixX*pixY/(sizeX*sizeY)
+	mm2PerArcsec2 = sizeX*sizeY/(scale*scale)
+	
 	if info == 1:
 		coma.info()
 		print repr(head)
 	
 	comaRegName = "POSIIF_Coma.reg"
-	comaReg = pyregion.open(comaRegName).as_imagecoord(head)
+	comaReg = pyregion.open(comaRegName)#.as_imagecoord(head)
 
 	for reg in comaReg:
 		center_xy = reg.coord_list[0], reg.coord_list[1]
 		radius = reg.coord_list[2]
-		newCenter, newRadius = centroid(center_xy,radius,dat)
+		brightCenter = centroid_bright(center_xy,radius,dat)
+		newCenter = centroid_weighted(center_xy,radius,dat)
+		newRadius = getRadius(newCenter, radius, dat)
 		reg.coord_list[0], reg.coord_list[1] = newCenter
 		reg.coord_list[2] = newRadius
-	
+		flux = getFlux(newCenter, newRadius, dat)*pix2Permm2*mm2PerArcsec2
+		print 'flux is ', ("{:.2e}".format(flux)), ' centered on ', center_xy
+		profile, err, r = makeProfile(newCenter, newRadius, dat)
+		profile = profile * pix2Permm2*mm2PerArcsec2
+		err = err * pix2Permm2*mm2PerArcsec2
+		r = r*pixX/sizeX*scale
+		plt.errorbar(r,profile,err)
+		plt.xlabel('radius (in arcsec)')
+		plt.ylabel('counts/arcsec^2')
+		plt.show()
+		for rr, rVal in enumerate(r):
+			maxFlux = max(profile)
+			if profile[rr] <= 0.5*maxFlux:
+				halfLight = rVal
+				break
+			halfLight = max(r)
+		print halfLight
+				
+		
 	maxval = numpy.amax(dat)
 	ax=plt.subplot(111)
 	ax.imshow(dat,cmap=cm.gray, vmin=0., vmax=maxval, origin="lower")
@@ -87,6 +181,7 @@ def main():
 		ax.add_patch(p)
 	for t in text_list:
 		ax.add_artist(t)
+
 	plt.show()
 	
 	coma.close()
